f3d00dcedda7272fc04048ff61d24f701c6d891b
nelsonlfranca <nelsonleandro8888@gmail.com>
2024-07-25 14:26:26 -0300

    .

---
+++ Exercícios/Ex023/js.js
@@ -0,0 +1,22 @@
+// let idade = 15;
+// let podeVotar = (idade >= 18) ? 'Sim' : 'Não';
+// console.log(podeVotar);
+
+// for (let i = 0; i < 10; i++) {
+//     if (i === 10) {
+//         break; // Sai do loop quando i é 5
+//     }
+//     console.log(i); // Imprime 0, 1, 2, 3, 4
+// }
+
+// function saudacao(Hau) {
+//     console.log(`Olá, ${Hau}!`);
+// }
+
+// saudacao('Hello'); // Imprime 'Olá, João!'
+//  let saudacao = (nome) => console.log(`Olá, ${nome}!`)
+//     saudacao('José')3
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i); // Imprime 0, 1, 2, 3, 4
+}
